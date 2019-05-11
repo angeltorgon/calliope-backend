1f72af28eaf3ac11027d6a3a540712b6dd27c4bf
angel-torres <alas4224@gmail.com>
2019-05-11 12:01:17 -0500

    add bcryptjs to passwords when signing up

--- data/migrations/20190509001254_users.js
+++ data/migrations/20190509001254_users.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments();
-    table.string('firstName', 255).unique().notNullable();
-    table.string('lastName', 255).unique().notNullable();
+    table.string('firstName', 255).notNullable();
+    table.string('lastName', 255).notNullable();
     table.string('username', 255).unique().notNullable();
     table.string('email', 255).unique().notNullable();
     table.string('password').notNullable();
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bcryptjs = require('bcryptjs');
 
 const authRoute = express.Router();
 
@@ -9,10 +9,13 @@ authRoute.post('/signup', async (req, res) => {
     const user = req.body;
     if (user.firstName && user.lastName && user.username && user.email && user.password ) {
         try {
+            const hash = bcryptjs.hashSync(user.password, 14);
+            user.password = hash;
             const [ id ] = await db('users').insert(user);
             res.status(200).json({id})
-        } catch (error) {
-            res.status(500).json({error})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({err})
         }
     }
 });
