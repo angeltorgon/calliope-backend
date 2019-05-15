54ac229e717b6ff301e01884fb7d336f18ef347a
angel-torres <alas4224@gmail.com>
2019-05-15 09:34:18 -0500

    fix comments migration file to have likes and to have text instead of string for comment

--- data/migrations/20190513173812_comments.js
+++ data/migrations/20190513173812_comments.js
@@ -1,14 +1,15 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
     return knex.schema.createTable('comment', table => {
         table.increments();
         table.integer('user_id', 255).notNullable().references('id').inTable('users').onDelete('CASCADE').index();
-        table.string('poemID').notNullable().references('id').inTable('poems').onDelete('CASCADE');
-        table.string('comment').notNullable();
+        table.string('poem_id').notNullable().references('id').inTable('poem').onDelete('CASCADE');
+        table.text('comment').notNullable();
+        table.integer('likes').notNullable().defaultsTo(0);
         table.timestamps(true, true);
-      })
+    })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
     return knex.schema.dropTableIfExists('comment');
 };
