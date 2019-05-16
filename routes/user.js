537249ec9cbc5dd237097750c21b08faeb3cc1e7
angel-torres <alas4224@gmail.com>
2019-05-16 10:45:22 -0500

    add put endpoint to user route

--- routes/user.js
+++ routes/user.js
@@ -24,6 +24,17 @@ userRoute.get('/:id', async (req, res) => {
         }
 });
 
+userRoute.put('/:id', async (req, res) => {
+    const user = req.body
+
+    try {
+        const updatedUser = await db('user').where({id: req.params.id}).update(user);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({error});
+    }
+});
+
 userRoute.delete('/:id', async (req, res) => {
     try {
         const deletedUser = await db('user').where({id: req.params.id}).delete();
