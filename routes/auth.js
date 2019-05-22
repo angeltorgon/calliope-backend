1d5cd3b223d092b72ba6dd92e1aac1244510c346
angel-torres <alas4224@gmail.com>
2019-05-21 20:21:01 -0500

    change error message in login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -41,7 +41,7 @@ authRoute.post('/login', (req, res) => {
         .then(data => {
             const user = data;
                 if ( user === undefined ) {
-                    res.status(400).json({message:"invalid credentials"});
+                    res.status(400).json({message:"User was not found"});
                 } else if ( bcryptjs.compareSync(password, user.password)) {
                     const token = generateToken(req.body);
                     res.status(200).json({ token, user});
