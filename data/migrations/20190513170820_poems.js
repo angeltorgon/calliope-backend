463f40f2d6bc88e50af2fc91d1039c4334de3a21
angel-torres <alas4224@gmail.com>
2019-05-30 16:55:48 -0500

    update poems seed file to include 20 lines for each poem

--- data/migrations/20190513170820_poems.js
+++ data/migrations/20190513170820_poems.js
@@ -3,11 +3,29 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('poem', table => {
     table.increments();
     table.string('poemTitle', 255).notNullable();
-    table.text('poem').notNullable();
     table.string('username', 255).notNullable().references('username').inTable('user').onDelete('CASCADE').index();
     table.integer('user_id').notNullable().references('id').inTable('user').onDelete('CASCADE').index();
     table.integer('likes').notNullable().defaultsTo(0);
     table.timestamps(true, true);
+    table.string('line_1');
+    table.string('line_2');
+    table.string('line_3');
+    table.string('line_4');
+    table.string('line_5');
+    table.string('line_6');
+    table.string('line_7');
+    table.string('line_8');
+    table.string('line_9');
+    table.string('line_10');
+    table.string('line_11');
+    table.string('line_12');
+    table.string('line_13');
+    table.string('line_14');
+    table.string('line_15');
+    table.string('line_16');
+    table.string('line_17');
+    table.string('line_18');
+    table.string('line_19');
   })
 };
 
