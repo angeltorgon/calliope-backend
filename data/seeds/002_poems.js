c1c96b2a21944eab00064301949e0b9489474c8f
angel-torres <alas4224@gmail.com>
2019-06-03 23:49:04 -0500

    let us see if this works

--- data/seeds/002_poems.js
+++ data/seeds/002_poems.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('poem').del()
+  return knex('poem').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('poem').insert([
