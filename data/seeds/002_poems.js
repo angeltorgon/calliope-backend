e001cf63a699633044c75ea5888c48d06059c829
angel-torres <alas4224@gmail.com>
2019-06-03 23:37:36 -0500

    refactor

--- data/seeds/002_poems.js
+++ data/seeds/002_poems.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('poem').truncate()
+  return knex('poem')
     .then(function () {
       // Inserts seed entries
       return knex('poem').insert([
--- data/seeds/003_comments.js
+++ data/seeds/003_comments.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('comment').truncate()
+  return knex('comment')
     .then(function () {
       // Inserts seed entries
       return knex('comment').insert([
